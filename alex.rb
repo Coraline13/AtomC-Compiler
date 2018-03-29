ca4f483035029427c6feb8c30ef0d6b4ea62d424
Unknown <coralia.bodea@gmail.com>
2018-03-29 11:34:14 +0300

    Add Alex class (analizator lexical)

---
+++ alex.rb
@@ -0,0 +1,49 @@
+require_relative 'token_type'
+
+class Alex
+    @tokens = Array.new
+
+    # line - current line in file
+    @line = 1
+    @current_position = 0
+
+    # read file
+    input_buffer = File.read("0.c")
+    puts input_buffer
+
+    private
+    def add_token(code, ct, line, column)
+        @tokens.push(Token.new(code, ct, line, column))
+    end
+
+    # err & token_err functions
+    private
+    def token_err(errors, tk = nil)
+        tk.nil? ? abort("Error: #{errors}") : abort("Error in line #{tk.line}: #{errors}")
+    end
+
+    private
+    def get_next_token
+        state = 0
+
+        loop do
+            case state
+            when 0
+                # ceva
+            when 1
+                # altceva
+            when 2
+
+            when 3
+
+            else
+                puts "ce???"
+            end
+        end
+    end
+
+    public
+    def show_tokens
+        @tokens.each {|tk| puts "#{tk.code}: #{tk.ct}"}
+    end
+end
